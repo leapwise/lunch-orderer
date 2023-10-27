da1246d2cba41769a056bfe6f7e91f19c1daab26
petra721 <petra-ilic@hotmail.com>
2023-10-27 09:07:59 +0200

    initial

---
+++ src/main/java/com/leapwise/lunchorderer/LunchOrdererApplication.java
@@ -0,0 +1,13 @@
+package com.leapwise.lunchorderer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchOrdererApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchOrdererApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leapwise/lunchorderer/LunchOrdererApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leapwise.lunchorderer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchOrdererApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
